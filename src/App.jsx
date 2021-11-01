cb01db53d8ca751aabbf91356d2410257d0e1db8
NguyenHero <mri.hannibal@gmail.com>
2021-11-01 17:26:19 +0700

    đ

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,72 @@ import React from 'react';
 import Album from './components/album';
 import Map from './components/map'
 import Info from './components/info'
+import { useRef, useState } from 'react';
+
 
 function App() {
-  const onScroll = ()=> {
-
+  const fullPage = useRef();
+  const [page, setPage] = useState(0);
+  const onScroll = (e) => {
+    const scrollTop = fullPage.current.scrollTop;
+    const height = 500;
+    const newPage = Math.floor(scrollTop /height);
+    if (newPage !== page) {
+      setPage(newPage);
+    }
   }
   return (
-    <div className="App">
+    <div className="App" ref={fullPage} onScroll={onScroll}>
       <div className="left-panel">
-        <Info/>
+        <div className="menu">
+          <div className="menu-btn">
+            <img alt="menu" src="https://img.icons8.com/color/48/ffffff/circled-menu.png" />
+          </div>
+          <div className="menu-panel">
+            <div className="item">
+              <img alt="album" src="https://img.icons8.com/color/48/ffffff/google-photos-new.png" />
+              <span>Album ảnh</span>
+            </div>
+            <div className="item">
+              <img alt="calendar" src="https://img.icons8.com/color/48/ffffff/google-calendar--v1.png" />
+              <span>Lịch trình</span>
+            </div>
+            <div className="item">
+              <img alt="map" src="https://img.icons8.com/color/48/ffffff/google-maps.png" />
+              <span>Địa chỉ</span>
+            </div>
+            <div className="item">
+              <img alt="contact" src="https://img.icons8.com/color/48/ffffff/apple-phone.png" />
+              <span>Liên hệ</span>
+            </div>
+
+          </div>
+        </div>
+        <Info page={page} />
+
       </div>
-      <div className="right-panel" onScroll={onScroll}>
+      <div className={`right-panel`} >
+        <div className="element page1">
+          page0
+        </div>
+        <div className="element ">
+          <div className="block-tile">Địa chỉ  </div>
+          <Map />
+        </div>
+        <div className="element">
+          <div className="block-tile">Lịch trình</div>
+        </div>
+        <div className="element ">
+          <div className="block-tile">  Liên hệ</div>
+
+        </div>
+        <div className="element">
+          <div className="block-tile">Album ảnh</div>
+          <Album/>
+        </div>
+        <div className="element">
+
+        </div>
 
       </div>
     </div>
--- src/components/info/index.js
+++ src/components/info/index.js
@@ -2,17 +2,17 @@
 import "./index.scss";
 import React from 'react';
 import Countdown from 'react-countdown';
+import IMAGES from '../../consts/data';
 
-const Info = () => {
+const Info = ({ page }) => {
+    const selectedImage = IMAGES[Math.floor(Math.random() * IMAGES.length)];
+    const renderInfo = () => {
 
-
-    const renderInfo = (info) => {
-        switch (info) {
-            case "summary":
+        switch (page) {
+            case 0:
                 return (
-                    <div className="block summary">
+                    <div className="block summary  animate__animated animate__fadeInRight">
                         <div className="title-info">
-                           
                             <div>
                                 Nguyễn
                                 <span className="and">&</span>
@@ -26,28 +26,62 @@ const Info = () => {
                         </div>
                     </div>
                 )
-            case "date-countdown":
+            case 1:
+                return (
+                    <div className="block summary  animate__animated animate__fadeInUp">
+                        <div className="title-info">
+
+                            <div>
+                                Nhà trại
+                            </div>
+
+                        </div>
+                        <div className="sub-info">
+                            Yên Tập - Nhân Hòa - Mỹ Hào - Hưng Yên
+                        </div>
+                    </div>
+                )
+            case 2:
+                return (
+                    <div className="block summary  animate__animated animate__fadeInDown">
+                        <div className="title-info">
+
+                            <div>
+                                Nhà gái
+                            </div>
+
+                        </div>
+                        <div className="sub-info">
+                            Chi Đoan - Cộng Hòa - Nam Sách - Hải Dương
+                        </div>
+                    </div>
+                )
+            case 3:
                 const Completionist = () => <span>Đã kết hôn!</span>;
                 const renderCountDown = (e) => {
                     console.log(e);
-                    return <div className="countdown block">
-                        <div className="item days">
-                            <div className="val">{e.days}</div>
-                            <div className="sub">Ngày</div>
+                    return (
+                        <div className="block countdown  animate__animated animate__fadeInLeft">
+                            <div className="item days">
+                                <div className="val">{e.days}</div>
+                                <div className="sub">Ngày</div>
+                            </div>
+                            <div className="item hours">
+                                <div className="val">{e.hours}</div>
+                                <div className="sub">Giờ</div>
+                            </div>
+                            <div className="item minutes">
+                                <div className="val">{e.minutes}</div>
+                                <div className="sub">Phút</div>
+                            </div>
+                            <div className="item seconds">
+                                <div className="val">{e.seconds}</div>
+                                <div className="sub">Giây</div>
+                            </div>
                         </div>
-                        <div className="item hours">
-                            <div className="val">{e.hours}</div>
-                            <div className="sub">Giờ</div>
-                        </div>
-                        <div className="item minutes">
-                            <div className="val">{e.minutes}</div>
-                            <div className="sub">Phút</div>
-                        </div>
-                        <div className="item seconds">
-                            <div className="val">{e.seconds}</div>
-                            <div className="sub">Giây</div>
-                        </div>
-                    </div>
+
+                    )
+
                 }
                 return (
                     < >
@@ -56,16 +90,14 @@ const Info = () => {
                         </Countdown>
                     </>
                 )
-
             default:
                 break;
         }
     }
     return (
-        <div className="info">
-            {renderInfo("summary")}
+        <div className="info" style={{ backgroundImage: `url(${selectedImage?.src})` }}>
+            {renderInfo()}
         </div>
-
     );
 }
 export default React.memo(Info);
