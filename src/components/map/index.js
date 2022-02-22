9f1420acff19bd2da4c486a6470a171cfc0b906b
Nguyễn <nguyenbv1994@gmail.com>
2022-02-22 14:32:14 +0700

    Update index.js

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -53,7 +53,7 @@ const Map = () => {
   };
 
   const mapEmbed = `
-    <iframe src="https://www.google.com/maps/embed?pb=!1m25!1m12!1m3!1d119216.47834018533!2d106.15369785771657!3d20.971985537748356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m10!3e0!4m4!2s21.01493%2C106.388990!3m2!1d21.01493!2d106.38898999999999!4m3!3m2!1d20.951629!2d106.05869299999999!5e0!3m2!1sen!2s!4v1644307854710!5m2!1sen!2s" width="600" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m25!1m12!1m3!1d119216.47834018533!2d106.15369785771657!3d20.971985537748356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m10!3e0!4m4!2s21.01493%2C106.388990!3m2!1d21.01493!2d106.38898999999999!4m3!3m2!1d20.951629!2d106.05869299999999!5e0!3m2!1sen!2s!4v1644307854710!5m2!1sen!2s" width="600" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     `
   return (
     <div>
